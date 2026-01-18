d23c9b2050b7b88c43714feb9fddbe63c7f864f1
hritikbk07 <hritikkami56@gmail.com>
2026-01-18 19:30:56 +0545

    frotend setup for eduverse

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        EduVerse 🚀
+      </h1>
+      <p className="mt-2 text-gray-600">
+        Learn. Build. Grow.
+      </p>
+    </main>
+  )
+}
